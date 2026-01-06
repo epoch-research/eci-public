d6eacd00f787e32bb53a3355d81766103934a2ee
luke.a.frymire <luke.a.frymire@gmail.com>
2026-01-05 20:39:25 -0800

    Add dataloader and tests

--- src/eci/__init__.py
+++ src/eci/__init__.py
@@ -6,6 +6,13 @@ ECI scores (model capabilities) and EDI scores (benchmark difficulties).
 """
 
 from .fitting import fit_eci_model, load_benchmark_data, compute_eci_scores
+from .dataloader import prepare_benchmark_data, download_benchmark_data
 
-__all__ = ["fit_eci_model", "load_benchmark_data", "compute_eci_scores"]
+__all__ = [
+    "fit_eci_model",
+    "load_benchmark_data",
+    "compute_eci_scores",
+    "prepare_benchmark_data",
+    "download_benchmark_data",
+]
 __version__ = "0.1.0"
